78444631515c7f58e159c989d49b7172852256ab
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-02-25 12:37:37 +0000

    urls created and firsts tests created

--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -12,7 +12,7 @@ def departmentApi(request, id=0):
     if request.method== 'GET':
         departments = Departments.objects.all()
         departments_serializer = DepartmentSerializer(departments, many=True)
-        return JsonResponse(departments_serializer, safe=False) 
+        return JsonResponse(departments_serializer.data, safe=False) 
     elif request.method == 'POST':
         departments_data = JSONParser().parse(request)
         departments_serializer = DepartmentSerializer(data=departments_data)
