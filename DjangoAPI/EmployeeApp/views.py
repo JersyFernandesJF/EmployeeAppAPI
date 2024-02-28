53fcc8b95fbbda245e003d20477d2032b35f7500
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-02-28 01:11:25 +0000

    added new feature on EmployeeAPI

--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -27,9 +27,35 @@ def departmentApi(request, id=0):
         if departments_serializer.is_valid():
             departments_serializer.save()
             return JsonResponse("Update Successfully", safe=False)
-        return JsonResponse("Failedto Update")
+        return JsonResponse("Failed to Update")
     elif request.method == 'DELETE':
         department = Departments.objects.get(DepartmentId = id)
         department.delete()
+        return JsonResponse("Deleted Successfully", safe=False)
+    
+@csrf_exempt
+def employeeApi(request, id = 0):
+    if request.method == 'GET':
+        employee = Employees.objects.all()
+        employee_serializer = EmployeeSerializer(employee, many = True)
+        return JsonResponse(employee_serializer.data, safe=False)
+    elif request.method == 'POST':
+        employee_data = JSONParser().parse(request)
+        employee_serializer = EmployeeSerializer(data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Added Successfully", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif request.method == 'PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(EmployeeId = employee_data['EmployeeId'])
+        employee_serializer = EmployeeSerializer(employee, data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Update Successfully", safe=False)
+        return JsonResponse("Failed to Update")
+    elif request.method == 'DELETE':
+        employee = Employees.objects.get(EmployeeId = id)
+        employee.delete()
         return JsonResponse("Deleted Successfully", safe=False)
     
